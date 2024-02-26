d59b3f4ded135cc7f584ce11a3d03e9b93a84c6f
GabePurc <gcpurcell6@gmail.com>
2024-02-26 11:19:54 -0500

    Added splined waypoint path for movement
    - Created a path with Bezier curve
    - Still need to add pause on user input

---
+++ Assets/_Scripts/PauseMovementOnInput.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace SWS
+{
+    public class PauseMovementOnInput : MonoBehaviour
+    {
+        public InputActionProperty pauseAction;
+
+        public splineMove mover;
+
+        private void Update()
+        {
+            if (pauseAction.action.ReadValue<float>()> 0.1f)
+            {
+                mover.Stop();
+            }
+        }
+    }
+
+}
+
