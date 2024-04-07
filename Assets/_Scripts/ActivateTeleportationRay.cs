bc7ea77c9df65341e2eb9a77027fdc6f3c658110
GabePurc <gcpurcell6@gmail.com>
2024-04-07 17:40:40 -0400

    Trying to fix teleportation movement
    - For some reason it is teleporting when I press the grip button even though I set it up to teleport when pressing forward on the left thumbstick

--- Assets/_Scripts/ActivateTeleportationRay.cs
+++ Assets/_Scripts/ActivateTeleportationRay.cs
@@ -7,15 +7,12 @@ using UnityEngine.InputSystem;
 public class ActivateTeleportationRay : MonoBehaviour
 {
     public GameObject leftTeleportation;
-    public GameObject rightTeleportation;
 
     public InputActionProperty leftActivate;
-    public InputActionProperty rightActivate;
 
 
     void Update()
     {
-        leftTeleportation.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
-        rightTeleportation.SetActive(rightActivate.action.ReadValue<float>() > 0.1f);
+        leftTeleportation.SetActive(leftActivate.action.ReadValue<Vector2>().y > 0.1f);
     }
 }
