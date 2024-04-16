96286fe004a2c97e437bc030b1d25d91668bc3d9
Lei Zhang <rockassical@gmail.com>
2024-04-16 00:48:57 -0400

    Added MRN complex animation

--- Assets/Scripts/XRControllerInput.cs
+++ Assets/Scripts/XRControllerInput.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using SWS;
 
 public class XRControllerInput : MonoBehaviour
 {
@@ -12,11 +13,21 @@ public class XRControllerInput : MonoBehaviour
         Right_Controller,
     }
 
+    private enum ButtonPress
+    {
+        ButtonUp,
+        ButtonDown,
+        ButtonHeld,
+    }
+
     [SerializeField]
     ControllerSide m_controller;
     InputDeviceCharacteristics m_characteristics;
 
-    
+    public splineMove splineTravel;
+    bool isButtonPressed;
+    bool isPaused = false; 
+    ButtonPress xButton = ButtonPress.ButtonUp; // Number corrisponding to the state of the x button on the left controller   
 
     // Start is called before the first frame update
     void Start()
@@ -42,25 +53,55 @@ public class XRControllerInput : MonoBehaviour
         }
         else
         {
-            Debug.Log("Controller not found");
+            //Debug.Log("Controller not found");
         }
+
+       
+        ///toggle player stop and go movement on the spline
+        if(!isPaused && xButton == ButtonPress.ButtonDown)
+        {
+            splineTravel.Pause();
+            isPaused = true;
+
+            Debug.Log("Pausing spline move");
+        }
+        else if (isPaused && xButton == ButtonPress.ButtonDown)
+        {
+            splineTravel.Resume();
+            isPaused = false;
+
+            Debug.Log("Unpausing spline move");
+        }
+
+
+
     }
 
     void CheckController(InputDevice d)
     {
         bool primaryButtonDown = false;
         d.TryGetFeatureValue(CommonUsages.primaryButton, out primaryButtonDown);
-        if (primaryButtonDown)
+
+        
+        if (primaryButtonDown) /// "X" button on left controller or "A" button on right controller
         {
             Debug.Log("Button down");
+            xButton++;
+
+            if ((int)xButton > 1)
+            {
+                xButton = ButtonPress.ButtonHeld;
+            }
         }
         else
         {
             Debug.Log("Button up");
+            xButton = ButtonPress.ButtonUp;
         }
 
         bool triggerDown = false;
         d.TryGetFeatureValue(CommonUsages.triggerButton, out triggerDown);
+
         if (triggerDown)
         {
             Debug.Log("Trigger down");
@@ -72,3 +113,34 @@ public class XRControllerInput : MonoBehaviour
     }
 
 }
+
+/*
+/// Check if the primary button (e.g., A or X) is pressed
+        if (controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
+        {
+            Debug.Log("Primary button pressed");
+            // Add your logic here for when the primary button is pressed
+        }
+
+/// Check if the secondary button (e.g., B or Y) is pressed
+if (controller.inputDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out bool secondaryButtonValue) && secondaryButtonValue)
+{
+    Debug.Log("Secondary button pressed");
+    // Add your logic here for when the secondary button is pressed
+}
+
+/// Check if the trigger button is pressed
+if (controller.inputDevice.TryGetFeatureValue(CommonUsages.triggerButton, out bool triggerButtonValue) && triggerButtonValue)
+{
+    Debug.Log("Trigger button pressed");
+    // Add your logic here for when the trigger button is pressed
+}
+
+    // Check if the grip button is pressed
+    if (controller.inputDevice.TryGetFeatureValue(CommonUsages.gripButton, out bool gripButtonValue) && gripButtonValue)
+    {
+     Debug.Log("Grip button pressed");
+     // Add your logic here for when the grip button is pressed
+    }
+
+*/
